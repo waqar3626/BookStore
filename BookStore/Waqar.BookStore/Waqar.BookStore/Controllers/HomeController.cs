29d66effdde67ea3797d17f70fe323b0ea816b17
waqar3626 <55612827+waqar3626@users.noreply.github.com>
2020-08-22 17:12:59 +0500

    -----

--- BookStore/Waqar.BookStore/Waqar.BookStore/Controllers/HomeController.cs
+++ BookStore/Waqar.BookStore/Waqar.BookStore/Controllers/HomeController.cs
@@ -8,9 +8,13 @@ namespace Waqar.BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index() {
+        public ViewResult Index() {
 
-            return "Welcome Waqar";
+            return View();
+        }
+        public ViewResult AboutUs()
+        {
+            return View();
         }
     }
 }
